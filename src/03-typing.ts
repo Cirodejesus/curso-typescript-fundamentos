a07fe43c196cebcb9aa78bbaa9717a6d4a8b9bab
Cirodejesus <cjarrieta2507@hotmail.com>
2023-10-13 18:34:06 -0500

    inferred types

---
+++ src/03-typing.ts
@@ -0,0 +1,16 @@
+(() => {
+//Tipos inferidos
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = 'change';
+
+myProductName.toUpperCase();
+
+myProductName.toString();
+
+const myProductStock = 1000;
+
+
+const myProductName2 = 'Product 1';
+})();
